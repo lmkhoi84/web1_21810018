310b6a9e6c4c8af8acf0daccd106665d1a1b4666
lmkhoi <khoi.leminh.stds@gmail.com>
2024-04-15 20:48:07 +0700

    full index

---
+++ js/21810018.js
@@ -0,0 +1,16 @@
+const API = 'https://web1-api.vercel.app/api';
+
+async function loadData(request,templateId,viewId){
+    const response = await fetch(`${API}/${request}`);
+    const data = await response.json();
+    //console.log(data);
+
+    var source = document.getElementById(templateId).innerHTML;
+    var template = Handlebars.compile(source);
+    //var context = {data : data};
+    var view = document.getElementById(viewId);
+    view.innerHTML = template({data:data});
+
+
+    //console.log(html);
+}
