177847e07cd66ba60c3aec8573156ab0cc577972
21810018 <khoi.leminh.stds@gmail.com>
2024-04-17 16:26:37 +0700

    nothing

--- api/verify.php
+++ api/verify.php
@@ -2,7 +2,7 @@
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 if (isset($_POST) && isset($_POST['g-token'])) {
-    $secretKey = '6LeR9bwpAAAAAHU-oncHFpRc-uBWQN3zxQrVANQ-';
+    $secretKey = '6LdYZ70pAAAAAJPzzjVn8gcwJLBby3JPQRnq-3Jn';
     $token = $_POST['g-token'];
     $ip = $_SERVER['REMOTE_ADDR'];
 
