e31163b4b107f65b44e1107cecb5d57229e4aee2
lmkhoi <khoi.leminh.stds@gmail.com>
2024-04-18 11:08:34 +0700

    modify verify.php

--- verify.php
+++ verify.php
@@ -11,9 +11,9 @@ if (isset($_POST) && isset($_POST['g-token'])) {
     $response = json_decode($request); 
 
     if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
-        header('HTTP/1.1 200 OK');
+        header('HTTPS/1.1 200 OK');
     } else {
-        header('HTTP/1.1 401 Unauthorized');
+        header('HTTPS/1.1 401 Unauthorized');
     }
     echo json_encode($response);
     exit();
